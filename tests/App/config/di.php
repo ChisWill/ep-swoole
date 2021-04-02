83726272942efb42ccd9681d85ba8e04d63a706d
CHISWILL-HOST\freed <freedomyky@163.com>
2021-04-03 00:04:48 +0800

    fix

---
+++ tests/App/config/di.php
@@ -0,0 +1,8 @@
+<?php
+
+declare(strict_types=1);
+
+use Ep\Swoole\Config;
+
+return static fn (Config $config): array => [
+];
--- tests/App/config/main.php
+++ tests/App/config/main.php
@@ -13,6 +13,7 @@ return [
     'env' => 'test',
     'debug' => true,
     'secretKey' => '8FA893E11FWE2340C6A68663217A181',
+    'di' => require('di.php'),
     'route' => require('route.php'),
     'events' => require('events.php'),
     'params' => require('params.php')
