04e2bbb08a7290c79d2f7d15150f0f88ee1c3bf7
ChisWill <459967016@qq.com>
2021-09-05 12:29:31 +0800

    v0.6.13

--- tests/App/bin/swoole.php
+++ tests/App/bin/swoole.php
@@ -7,17 +7,8 @@ use Ep\Swoole\Application;
 
 require(dirname(__DIR__, 3) . '/vendor/autoload.php');
 
-Ep::init(array_merge(
-    file_exists(dirname(__DIR__) . '/config/main-local.php') ?
-        array_merge(
-            require(dirname(__DIR__) . '/config/main.php'),
-            require(dirname(__DIR__) . '/config/main-local.php')
-        ) :
-        require(dirname(__DIR__) . '/config/main.php'),
-));
-
-$exitCode = Ep::getDi()
+$exitCode = Ep::init(import('main'))
     ->get(Application::class)
-    ->run(require(dirname(__DIR__) . '/config/swoole.php'));
+    ->run(import('swoole'));
 
 exit($exitCode);
